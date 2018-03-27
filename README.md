1d643be74b563b7a1d030fea4ba7cedd1eba1409
Mikhail Popov <mpopov@wikimedia.org>
2018-03-27 09:38:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Google Search Consola Data Fetcher
+
+Wikipedia's and other Wikimedia Foundation sites' presence in Google's search results via [Search Console API](https://developers.google.com/webmaster-tools/search-console-api-original/).
+
+## Setup
+
+Relies on the [Google's Python client library](https://developers.google.com/api-client-library/python/) via [google-api-python-client](https://github.com/google/google-api-python-client):
+
+```bash
+pip install -r requirements.txt
+```
+
+### Credentials
+
+Adapted from [Quickstart: Run a Search Console App in Python ](https://developers.google.com/webmaster-tools/search-console-api-original/v3/quickstart/quickstart-python) and [OAuth 2.0 Storage](https://developers.google.com/api-client-library/python/guide/aaa_oauth#storage):
+
+```python
+import httplib2
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+
+flow = flow_from_clientsecrets('client_secrets.json', scope='https://www.googleapis.com/auth/webmasters.readonly', redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+
+# Create an httplib2.Http object and authorize it with our credentials
+authorize_url = flow.step1_get_authorize_url()
+print('Go to the following link in your browser: ' + authorize_url)
+code = input('Enter verification code: ').strip()
+credentials = flow.step2_exchange(code)
+http = httplib2.Http(cache="cache")
+http = credentials.authorize(http)
+
+# Save
+storage = Storage('cache/credentials.json')
+storage.put(credentials)
+```
+
+You should have a file that looks like [this](cache/credentials-sample.json).
+
+## Usage
+
+```bash
+python3 fetch_data.py path/to/credentials SITE SPLIT path/to/output DATE DAYS
+```
+
+**Note**: that since Google doesn't store more than 90 days of Search Console reports, be careful choosing `DATE` and `DAYS`.
+
+HTTPS is assumed but an HTTP variant of a property is supported:
+
+```bash
+python3 fetch_data.py --http path/to/credentials SITE SPLIT path/to/output
+```
+
+See `./fetch_data.py --help` for more information.
+
+### Example
+
+```bash
+virtualenv -p /usr/bin/python3 myenv
+source myenv/bin/activate
+pip install -U -r gsc/requirements.txt
+```
+
+```bash
+# source myenv/bin/activate
+# cd gsc
+python fetch_data.py cache/creds-mpopov.json en.wikipedia.org none output/enwiki 2018-03-24 90
+```
---
+++ fetch_data.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import datetime as dt
+import argparse
+import httplib2
+import numpy as np
+import pandas as pd
+
+from apiclient import errors
+from apiclient.discovery import build
+from oauth2client.file import Storage
+
+# Helper function:
+def critical(message):
+    print('ERROR: ' + message)
+    sys.exit(1)
+
+# Establish command-line arguments:
+parser = argparse.ArgumentParser(description='Fetch Google Search Console statistics for a property.')
+parser.add_argument("credentials", help="Use custom credentials file")
+parser.add_argument("website",
+                    help="Web property to fetch stats about; e.g. en.wikipedia.org, commons.wikimedia.org")
+parser.add_argument("split", choices=['none', 'country', 'device', 'appearance'], default='none',
+                    help="Dimension to split by, if any")
+parser.add_argument("outdir", help="path/to/directory to store output in")
+parser.add_argument("date", nargs="?", default=dt.date.today().strftime("%Y-%m-%d"),
+                    help="Date (YYYY-MM-DD) to fetch data for (default: %(default)s (today))")
+parser.add_argument("days", type=int, default=0, nargs="?",
+                    help="Days to fetch, up to 90; useful for back-filling (default: %(default)s)")
+parser.add_argument("--http", action="store_true",
+                    help="Fetch stats about the non-secure variant (https is used by default)")
+parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+args = parser.parse_args()
+# dev/debug: ['-v', 'cache/creds-mpopov.json', 'en.wikipedia.org', 'none', 'output/enwiki', '2018-03-01', '90']
+
+# Create the URL to be used with the request:
+if args.http:
+    website_url = "http://" + args.website + '/'
+else:
+    website_url = "https://" + args.website + '/'
+
+# Create the start of the data range:
+try:
+    start_date = dt.datetime.strptime(args.date, '%Y-%m-%d') - dt.timedelta(days=args.days)
+except ValueError:
+    critical('Invalid %s.' % args.date)
+
+# Create an httplib2.Http object and authorize it with saved credentials:
+if args.verbose:
+    print("Loading credentials from " + args.credentials)
+
+storage = Storage(args.credentials)
+credentials = storage.get()
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+# refer to developers.google.com/resources/api-libraries/documentation/webmasters/v3/python/latest/
+#   webmasters_v3.searchanalytics.html for more information
+webmasters_service = build('webmasters', 'v3', http=http)
+def execute_request(site_url, request):
+   return webmasters_service.searchanalytics().query(siteUrl = site_url, body = request).execute()
+
+# Construct API request:
+request = {
+    'startDate': start_date.strftime("%Y-%m-%d"),
+    'endDate': args.date
+}
+if args.split == 'country':
+    request['dimensions'] = ['date', 'country']
+elif args.split == 'device':
+    request['dimensions'] = ['date', 'device']
+elif args.split == 'appearance':
+    request['dimensions'] = ['date']
+    request['dimensionFilterGroups'] = [{
+            'filters': [{
+                'dimension': 'searchAppearance',
+                'expression': 'RICHCARD'
+            }]
+    }]
+else:
+    request['dimensions'] = ['date']
+
+if args.verbose:
+    print("Requesting the following info about '" + website_url + "':")
+    print(request)
+
+response = execute_request(website_url, request)
+
+if not response.__contains__('rows'):
+    critical('No data returned by the API')
+
+# Get a DataFrame into a writeable format:
+df = pd.DataFrame.from_dict(response['rows'])
+df['date'] = df['keys'].apply(lambda x: x[0])
+df['clicks'] = df['clicks'].astype(np.int64)
+df['impressions'] = df['impressions'].astype(np.int64)
+
+if args.split == 'country':
+    df['country'] = df['keys'].apply(lambda x: x[1].upper())
+    df = df[['date', 'country', 'clicks', 'impressions', 'ctr', 'position']]
+    df = df.sort_values(by = ['date', 'country'], ascending=[True, True])
+elif args.split == 'device':
+    df['device'] = df['keys'].apply(lambda x: x[1].capitalize())
+    df = df[['date', 'device', 'clicks', 'impressions', 'ctr', 'position']]
+    df = df.sort_values(by=['date', 'device'], ascending=[True, True])
+else:
+    df = df[['date', 'clicks', 'impressions', 'ctr', 'position']].sort_values(by = ['date'], ascending=True)
+
+# Create output directory if it does not exist yet:
+if not os.path.exists(args.outdir):
+    os.makedirs(args.outdir)
+
+output_filename = args.outdir + '/' + args.split + '.csv'
+
+df.to_csv(output_filename, index=False, float_format='%.4f', mode='a', header=not os.path.exists(output_filename))
